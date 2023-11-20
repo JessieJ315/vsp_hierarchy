7a5453b590c2569db881a8e1d1e91f2dad811015
JessieJ315 <58019628+JessieJ315@users.noreply.github.com>
2023-11-20 23:26:00 +0000

    Update output_analysis.R

--- output_analysis.R
+++ output_analysis.R
@@ -31,9 +31,11 @@ abline(v = burn_in, col='red')
 plot(output$tau[1:N],type='l',main='latent matrix dispersion parameter',ylab='tau')
 abline(v=burn_in, col='red')
 ## paramters - noise model
-plot(output$p[1:N],type='l',main='error probability for the queue-jumping noise model',ylab='p')
+plot(output$p[1:N],type='l',main='error probability for the queue-jumping noise 
+     model',ylab='p')
 abline(v=burn_in, col='red')
-plot(output$theta[1:N],type='l',main="dispersion parameter for the Mallow's noise model",ylab='theta')
+plot(output$theta[1:N],type='l',main="dispersion parameter for the Mallow's noise 
+     model",ylab='theta')
 abline(v=burn_in, col='red')
 ## latent matrices
 U0_means=do.call(rbind,lapply(output$U0[1:N], function(m){apply(m,1,mean)}))
@@ -73,7 +75,8 @@ sort(PO_unlist,decreasing = TRUE)[1:5]
 PO_mp=str2po(names(which(PO_unlist==max(PO_unlist))),num_actors)
 par(mfrow=c(1,1))
 showDAG(transitive.reduction(PO_mp))
-showDAG(transitive.reduction(str2po(names(sort(PO_unlist,decreasing = TRUE)[2]),num_actors)))
+showDAG(transitive.reduction(str2po(names(sort(PO_unlist,decreasing = TRUE)[2]),
+                                    num_actors)))
 ### concensus order
 concensus_po = Reduce('+',global_pos[burn_in:N])/length(global_pos[burn_in:N]) # po_posterior transitive closure
 showDAGcon(concensus_po,threshold =.3)
@@ -88,7 +91,8 @@ for (i in 1:num_assessors){
   PO_mp=str2po(names(which(PO_unlist==max(PO_unlist))),num_actors)
   par(mfrow=c(1,1))
   showDAG(transitive.reduction(PO_mp))
-  showDAG(transitive.reduction(str2po(names(sort(PO_unlist,decreasing = TRUE)[2]),num_actors)))
+  showDAG(transitive.reduction(str2po(names(sort(PO_unlist,decreasing = TRUE)[2]),
+                                      num_actors)))
   ### concensus order
   concensus_po = Reduce('+',individual_pos[burn_in:N])/length(individual_pos[burn_in:N]) # po_posterior transitive closure
   showDAGcon(concensus_po,threshold =.3)
@@ -96,44 +100,53 @@ for (i in 1:num_assessors){
 
 # posterior distributions
 ## rho
-ggplot(melt(data.frame(prior=rbeta(N-burn_in,1,RHO_HYPERPAMA),posterior = output$rho[(burn_in+1):N])),aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
+ggplot(melt(data.frame(prior=rbeta(N-burn_in,1,RHO_HYPERPAMA),
+                       posterior = output$rho[(burn_in+1):N])),
+       aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
   geom_density(alpha=0.4,adjust = 3)+
   labs(title="Prior/Posterior Distributiosn for rho",
        y="", x = "rho")+
   scale_color_manual(values=c("#999999", "#0072B2"))+
   scale_linetype_manual(values=c('dashed','solid'))+
   theme_classic()+
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),legend.title=element_blank(),text=element_text(family='Times New Roman'))
+  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),
+        legend.title=element_blank(),text=element_text(family='Times New Roman'))
 mean(output$rho[burn_in:N])
 ## tau
-ggplot(melt(data.frame(prior=1/(1+exp(-rnorm(N-burn_in,sd=T_HYPERPAMA))),posterior = output$tau[(burn_in+1):N])),aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
+ggplot(melt(data.frame(prior=1/(1+exp(-rnorm(N-burn_in,sd=T_HYPERPAMA))),
+                       posterior = output$tau[(burn_in+1):N])),
+       aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
   geom_density(alpha=0.4,adjust = 3)+
   labs(title="Prior/Posterior distributiosn for tau",
        y="", x = "tau")+
   scale_color_manual(values=c("#999999", "#0072B2"))+
   scale_linetype_manual(values=c('dashed','solid'))+
   theme_classic()+
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),legend.title=element_blank(),text=element_text(family='Times New Roman'))
+  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),
+        legend.title=element_blank(),text=element_text(family='Times New Roman'))
 mean(output$tau[(burn_in+1):N])
 ## p
-ggplot(melt(data.frame(prior=1/(1+exp(-rnorm(N-burn_in,sd=R_HYPERPAMA))),posterior = output$p[(burn_in+1):N])),aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
+ggplot(melt(data.frame(prior=1/(1+exp(-rnorm(N-burn_in,sd=R_HYPERPAMA))),
+                       posterior = output$p[(burn_in+1):N])),
+       aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
   geom_density(alpha=0.4,adjust = 3)+
   labs(title="Prior/Posterior distributiosn for Error Probability p",y="", x = "p")+
   scale_color_manual(values=c("#999999", "#0072B2"))+
   scale_linetype_manual(values=c('dashed','solid'))+
   theme_classic()+
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),legend.title=element_blank(),text=element_text(family='Times New Roman'))
+  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),
+        legend.title=element_blank(),text=element_text(family='Times New Roman'))
 mean(output$p[(burn_in+1):N])
 ## theta
-ggplot(melt(data.frame(prior=rgamma(N-burn_in,shape=THETA_HYPERPAMA,rate=1),posterior = output$theta[(burn_in+1):N])),aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
+ggplot(melt(data.frame(prior=rgamma(N-burn_in,shape=THETA_HYPERPAMA,rate=1),
+                       posterior = output$theta[(burn_in+1):N])),
+       aes(x=value,y=..density..,group=variable,color=variable,linetype=variable))+
   geom_density(alpha=0.4,adjust = 3)+
   labs(title="Prior/Posterior distributiosn for theta",
        y="", x = "theta")+
   scale_color_manual(values=c("#999999", "#0072B2"))+
   scale_linetype_manual(values=c('dashed','solid'))+
   theme_classic()+
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),legend.title=element_blank(),text=element_text(family='Times New Roman'))
+  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5),
+        legend.title=element_blank(),text=element_text(family='Times New Roman'))
 mean(Result$theta[(burn_in+1):N])
-
-
-
